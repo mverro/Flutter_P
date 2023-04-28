ea8aa8297e2a3073feb79612a8a96de577dacd3a
Azzam Adnan <azzamuadonang@gmail.com>
2023-04-28 21:13:36 +0700

    World Time Mobile

---
+++ android/app/src/main/kotlin/com/example/world_time2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.world_time2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/loading.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:world_time2/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Jakarta', flag: 'indonesia.png', url: 'Asia/Jakarta');
+    await instance.getTime();
+
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue[900],
+        body: Center(
+          child: SpinKitThreeBounce(
+            color: Colors.white,
+            size: 50.0,
+          ),
+        ));
+  }
+}
